c28b17e3ffd8c4f7887769a513bbae2752a84a40
GenkoP <genko_petrov@hotmail.com>
2015-02-11 16:15:36 +0200

    Create web client MVC whit users

---
+++ Tasks.WebClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tasks.WebClient.Startup))]
+namespace Tasks.WebClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
