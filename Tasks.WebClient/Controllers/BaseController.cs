fcde759ca7e711e3a32b99bb3f34d50971b042f7
GenkoP <genko_petrov@hotmail.com>
2015-02-11 17:26:18 +0200

    Add dependency injection with ninject

---
+++ Tasks.WebClient/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿namespace Tasks.WebClient.Controllers
+{
+    using System.Web.Mvc;
+
+    using Tasks.Data.Repositories;
+    using Tasks.Data;
+
+    public abstract class BaseController : Controller
+    {
+
+        protected ITaskManagerData data;
+
+        public BaseController(ITaskManagerData data)
+        {
+            this.data = data;
+        }
+    }
+}
--- Tasks.WebClient/Controllers/HomeController.cs
+++ Tasks.WebClient/Controllers/HomeController.cs
@@ -1,22 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace Tasks.WebClient.Controllers
+﻿namespace Tasks.WebClient.Controllers
 {
-    public class HomeController : Controller
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using Tasks.Data.Repositories;
+
+    public class HomeController : BaseController
     {
+
+        public HomeController(ITaskManagerData data)
+            : base(data)
+        {
+
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var allTasks = this.data.Tasks.All();
+
+            return View(allTasks);
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
