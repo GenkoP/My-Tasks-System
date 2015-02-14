8a3fa7078780720e1f524627c9d845c563816653
GenkoP <genko_petrov@hotmail.com>
2015-02-14 20:24:25 +0200

    Implement creation new Tasks

---
+++ Tasks.WebClient/Controllers/TasksController.cs
@@ -0,0 +1,78 @@
+﻿namespace Tasks.WebClient.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using Tasks.Models;
+    using Tasks.Data.Repositories;
+    using Tasks.WebClient.Providers;
+    using Tasks.WebClient.Models.InputModels;
+    using Tasks.WebClient.Models.ViewModels;
+   
+
+
+    [Authorize]
+    public class TasksController : BaseController
+    {
+
+        public TasksController(ITaskManagerData data, ICurrentUserIdProvider userId)
+            : base(data, userId)
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var currentUserId = this.CurrentUser.GetUserId();
+
+            var currentTasks = this.Data.Tasks.All()
+                .Where(x => x.DateToEnd > DateTime.Now && x.UserID == currentUserId)
+                .OrderBy(x => x.Preority)
+                .Select(MyTaskViewModelHomeIndex.GetTasks);
+
+
+            return View(currentTasks);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(MyTaskInputModel task)
+        {
+
+
+            if (!ModelState.IsValid)
+            {
+                return View("Error");
+            }
+
+            var currentUserId = this.CurrentUser.GetUserId();
+
+            var newTask = new MyTask
+            {
+                UserID = currentUserId,
+                Title = task.Title,
+                DateOnCreate = DateTime.Now,
+                DateToEnd = task.DateToEnd,
+                Description = task.Description,
+                Preority = task.Preority,
+
+            };
+
+            this.Data.Tasks.Add(newTask);
+            this.Data.SaveChanges();
+
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Tasks.WebClient/Models/InputModels/MyTaskInputModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Tasks.WebClient.Models.InputModels
+{
+    using System;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
+    using Tasks.Models;
+
+
+    public class MyTaskInputModel
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 2)]
+        public string Title { get; set; }
+
+        [StringLength(600)]
+        public string Description { get; set; }
+
+        public DateTime DateOnCreate { get; set; }
+
+        public DateTime? DateToEnd { get; set; }
+
+       public PreorityType Preority { get; set; }
+
+        public string UserID { get; set; }
+    }
+}
