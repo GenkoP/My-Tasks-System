c0cd156d9faaf94d8343b5717c54129ecf9ad322
GenkoP <genko_petrov@hotmail.com>
2015-02-17 22:37:16 +0200

    Implement jquery ui datepicker in create and update view. Validate datetime pars from datepicker and min date current date .

--- Tasks.WebClient/App_Start/BundleConfig.cs
+++ Tasks.WebClient/App_Start/BundleConfig.cs
@@ -16,6 +16,8 @@ namespace Tasks.WebClient
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/date-val").Include(
+                "~/Scripts/additional/datepicar-validate.js"));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ Tasks.WebClient/Scripts/additional/datepicar-validate.js
@@ -0,0 +1,45 @@
+﻿jQuery(function ($) {
+
+    $('#DateToEnd')
+        .attr(
+        "data-val-date",
+        "The field Date to end: is incorect! Date must be a to bigger from current datetime or format is incorect! ");
+
+    $('#DateToEnd').datepicker({
+        dateFormat: 'dd/mm/yy',
+        minDate: new Date(),
+    }).val();
+
+
+    $.validator.addMethod("date",
+        function (value, element) {
+
+            var today = new Date(),
+                day = today.getDate(),
+                month = today.getMonth(),
+                year = today.getFullYear(),
+                curDate = new Date(year, month, day);
+            dateIsCorect = true;
+
+            if (this.optional(element)) {
+                return true;
+            }
+
+            try {
+                var inputDate = $.datepicker.parseDate('dd/mm/yy', value);
+            } catch (e) {
+                return false;
+            }
+
+            if (curDate > inputDate) {
+                dateIsCorect = false;
+            }
+            else {
+                dateIsCorect = true;
+            }
+            return dateIsCorect;
+        });
+
+
+
+});
