eb18370006f4de930d4d714d3e7ebf193c4216c4
GenkoP <genko_petrov@hotmail.com>
2015-02-20 01:18:27 +0200

    return test

---
+++ Tasks.Tests/ControllersTest/TestAddingDataInMyTasksController.cs
@@ -0,0 +1,56 @@
+﻿namespace Tasks.Tests.ControllersTest
+{
+
+    using System;
+    using System.Web.Mvc;
+    using System.Collections.Generic;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    
+    using Moq;
+
+    using Tasks.Data.Repositories;
+    using Tasks.Models;
+    using Tasks.WebClient.Models.InputModels;
+    using Tasks.WebClient.Controllers;
+
+
+
+    [TestClass]
+    public class TestAddingDataInMyTasksController : BaseTestController
+    {
+        [TestMethod]
+        public void CreateNewMyTasks_When_InputModelIsValid_AndSubtaskCollIsNull_ShouldRedirectToIndexPage()
+        {
+
+            var listOfMyTasks = new List<MyTask>();
+
+            var myTask = new MyTask
+            {
+                  Title = "mytask title",
+                 
+            };
+
+            this.MoqTaskManagerData.Setup(x => x.Tasks.Add(It.Is<MyTask>(c => true))).Callback(() =>
+            {
+                listOfMyTasks.Add(myTask);
+
+            });
+
+            var data = this.MoqTaskManagerData.Object;
+
+
+            var taskContr = new TasksController(data, this.CurrentUserIdProvider);
+
+            var taskInput = new MyTaskInputModel
+            {
+                 Title = "input title",
+            };
+
+            var action = taskContr.Create(taskInput, null);
+
+            Assert.IsTrue(listOfMyTasks.Contains(myTask));
+        }
+            
+    }
+}
